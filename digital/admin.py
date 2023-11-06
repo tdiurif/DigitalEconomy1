fb37ea077fa3c0f8c7afee5e8a86952232d0a4e5
baxtiyorgaybullayevich <135433427+baxtiyorgaybullayevich@users.noreply.github.com>
2023-11-06 15:59:01 +0500

    Initial commit

---
+++ digital/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import DepartamentModels, NewsModel, TeachModels, ImageModels, FacultetsModels, SlideModels, GroupModel, DaysModel, ScheduleModel
+
+admin.site.register(DepartamentModels)
+admin.site.register(NewsModel)
+admin.site.register(TeachModels)
+admin.site.register(ImageModels)
+admin.site.register(FacultetsModels)
+admin.site.register(SlideModels)
+admin.site.register(DaysModel)
+admin.site.register(ScheduleModel)
+admin.site.register(GroupModel)
---
+++ digital/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+
+
+class NewsModel(models.Model):
+    title = models.CharField(max_length=250, blank=True, null=True)
+    image = models.ImageField(upload_to='static/img/news/', blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    date_ad = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+
+class DepartamentModels(models.Model):
+    title = models.CharField(max_length=250, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(upload_to='static/img/departament/', blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} | {self.name}'
+
+
+class TeachModels(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='static/image/teach/', blank=True, null=True)
+
+    def __str__(self):
+        return f'Rasm {self.id}'
+
+
+class ImageModels(models.Model):
+    image = models.ImageField(upload_to='static/image/skrol/', blank=True, null=True)
+
+
+class FacultetsModels(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    body = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='static/image/facultets/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class SlideModels(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='static/image/slide/', blank=True, null=True)
+    url = models.CharField(max_length=255, blank=True, null=True)
+
+
+class DaysModel(models.Model):
+    day = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.day}'
+
+
+class GroupModel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ScheduleModel(models.Model):
+    day = models.ForeignKey(DaysModel, on_delete=models.CASCADE)
+    group = models.ForeignKey(GroupModel, on_delete=models.CASCADE)
+    sub1 = models.CharField(max_length=255, null=True, blank=True)
+    sub2 = models.CharField(max_length=255, null=True, blank=True)
+    sub3 = models.CharField(max_length=255, null=True, blank=True)
+    sub4 = models.CharField(max_length=255, null=True, blank=True)
+    time1 = models.TimeField(blank=True, null=True)
+    time2 = models.TimeField(blank=True, null=True)
+    time3 = models.TimeField(blank=True, null=True)
+    time4 = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.group}'
+
