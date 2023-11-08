b5ceac590712b7de54820293d9119cc263149857
root <bekmuradovulugbek25@gmail.com>
2023-11-08 14:43:10 +0500

    update izzat

--- digital/models.py
+++ digital/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 
 class NewsModel(models.Model):
     title = models.CharField(max_length=250, blank=True, null=True)
     image = models.ImageField(upload_to='static/img/news/', blank=True, null=True)
-    body = models.TextField(blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
     date_ad = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
 
 class DepartamentModels(models.Model):
-    title = models.CharField(max_length=250, blank=True, null=True)
+    title = models.CharField(max_length=250, blank=True, null=True, )
     name = models.CharField(max_length=255, blank=True, null=True)
     image = models.ImageField(upload_to='static/img/departament/', blank=True, null=True)
-    body = models.TextField(blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.title} | {self.name}'
@@ -20,7 +21,7 @@ class DepartamentModels(models.Model):
 
 class TeachModels(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
-    body = models.TextField(blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to='static/image/teach/', blank=True, null=True)
 
     def __str__(self):
@@ -45,7 +46,7 @@ class ImagePrevMOdels(models.Model):
 
 class FacultetsModels(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
-    body = models.TextField(blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to='static/image/facultets/', blank=True, null=True)
 
     def __str__(self):
@@ -54,7 +55,7 @@ class FacultetsModels(models.Model):
 
 class SlideModels(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
-    body = models.TextField(blank=True, null=True)
+    body = RichTextField(blank=True, null=True)
     image = models.ImageField(upload_to='static/image/slide/', blank=True, null=True)
     url = models.CharField(max_length=255, blank=True, null=True)
 
@@ -92,7 +93,7 @@ class ScheduleModel(models.Model):
 class ProfessorModel(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='static/img/professor/')
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     number = models.CharField(max_length=255, blank=True, null=True)
     room = models.CharField(max_length=255, null=True, blank=True)
@@ -104,7 +105,8 @@ class ProfessorModel(models.Model):
 class TeachersModel(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='static/img/professor/')
-    body = models.TextField()
+    position = models.ForeignKey(DepartamentModels, on_delete=models.PROTECT)
+    body = RichTextField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     number = models.CharField(max_length=255, blank=True, null=True)
     room = models.CharField(max_length=255, null=True, blank=True)
@@ -115,7 +117,28 @@ class TeachersModel(models.Model):
 
 class BitiruvchilarModel(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
-    ball = models.DecimalField(max_digits=10,decimal_places=10)
+    ball = models.DecimalField(max_digits=10, decimal_places=10)
 
     def __str__(self):
         return f'{self.name}   |    {self.ball}'
+
+
+class AlumniModels(models.Model):
+    name = models.CharField(null=True, blank=True, max_length=255)
+    position = models.CharField(null=True, blank=True, max_length=255)
+    info = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='static/img/alumni/')
+    comment = RichTextField(blank=True, null=True)
+    Teach = models.CharField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}    |    {self.position}'
+
+
+class Comment(models.Model):
+    title = models.CharField(blank=True, null=True, max_length=255)
+    body = RichTextField(null=True, blank=True)
+    image = models.ImageField(upload_to='static/img/comment/')
+
+    def __str__(self):
+        return f'{self.title}'
