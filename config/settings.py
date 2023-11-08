3b17bd473eae4e54241a8b4216b538fa1325d1a2
baxtiyorgaybullayevich <135433427+baxtiyorgaybullayevich@users.noreply.github.com>
2023-11-08 14:41:17 +0500

    update all

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'digital'
+    'digital',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
