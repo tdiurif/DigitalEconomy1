c980165a1bd7a29cd127f108691ed786e80754ff
baxtiyorgaybullayevich <135433427+baxtiyorgaybullayevich@users.noreply.github.com>
2023-11-08 18:28:12 +0500

    update all

--- digital/views.py
+++ digital/views.py
@@ -33,17 +33,16 @@ def TechView(request):
 
 
 def DepartamentView(request):
-    departaments=DepartamentModels.objects.all()
-    list1=[]
-    for i in range(1,len(departaments)+1):
-        if i%2==0:
+    departaments = DepartamentModels.objects.all()
+    list1 = []
+    for i in range(1, len(departaments) + 1):
+        if i % 2 == 0:
             list1.append(i)
 
-    return render(request, 'fiveindex.html',{
-        "departaments":departaments,
-        "list1":list1
+    return render(request, 'fiveindex.html', {
+        "departaments": departaments,
+        "list1": list1
     })
-
 
 
 def TadqiqotlarView(request):
@@ -61,5 +60,6 @@ def FacultySportView(request):
 def SocialProjectView(request):
     return render(request, 'sevenindex.html')
 
+
 def ScoreBallsView(request):
-    return render(request,'nineindex.html')
+    return render(request, 'nineindex.html')
